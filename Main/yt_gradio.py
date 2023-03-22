28e22e3b962e6f2a576d224356a0e4c1f311908d
AlekVolok <alek.volzok@gmail.com>
2023-03-22 23:30:26 +0100

    Update yt_gradio.py
    Add code for pytube autoupdate

--- Main/yt_gradio.py
+++ Main/yt_gradio.py
@@ -6,17 +6,21 @@ import sys
 try:
     import gradio as gr
     import ffmpeg
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "--upgrade", "pytube"])
     from pytube import YouTube as yt
     from moviepy.editor import VideoFileClip
+    from vimeo_downloader import Vimeo
 except:
     subprocess.check_call([sys.executable, "-m", "pip", "install", "ffmpeg"])
     subprocess.check_call([sys.executable, "-m", "pip", "install", "pytube"])
     subprocess.check_call([sys.executable, "-m", "pip", "install", "moviepy"])
     subprocess.check_call([sys.executable, "-m", "pip", "install", "gradio"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "vimeo_downloader"])
     import ffmpeg
     from pytube import YouTube as yt
     import gradio as gr
     from moviepy.editor import VideoFileClip
+    from vimeo_downloader import Vimeo
 
 
 def clean_folder(folder, filetype=None):
@@ -47,7 +51,7 @@ def convert_mp3(mp4_file):
 
 def download_youtube_video(url, output_folder):
     """
-    Downloads video from the giving url
+    Downloads youtube best video from the giving url
     Inputs:
         url (str): video url to download
         output_folder (str): folder path to download
@@ -59,6 +63,35 @@ def download_youtube_video(url, output_folder):
     downloaded_vid = hd_stream.download(output_folder)
 
     return downloaded_vid
+
+
+def download_vimeo_video(url, output_folder):
+    """
+    Downloads youtube best video from the giving url
+    Inputs:
+        url (str): video url to download
+        output_folder (str): folder path to download
+    Returns:
+        downloaded_vid (str): downloaded video's filepath
+    """
+    # Try to get video from url
+    try:
+        vimeo_video = Vimeo(url)
+    # If dosn't work - try do get from ID
+    except:
+        vimeo_video = Vimeo.from_video_id(url)
+
+    vimeo_video_title = vimeo_video.metadata.title
+    hd_stream = vimeo_video.streams[-1]
+
+    # Download video 
+    hd_stream.download(download_directory=output_folder, filename=vimeo_video_title)
+    
+    # Create output filepath
+    downloaded_vid = f"{output_folder}/{vimeo_video_title}.mp4"
+
+    return downloaded_vid
+
 
 
 def simple_download(url_paths, dl_path=None, convert_to_mp3=False):
@@ -76,7 +109,10 @@ def simple_download(url_paths, dl_path=None, convert_to_mp3=False):
     for url in urls:
         
         # Download videos and store their paths
-        mp4_filepath = download_youtube_video(url, dl_path)
+        try:
+            mp4_filepath = download_youtube_video(url, dl_path)
+        except:
+            mp4_filepath = download_vimeo_video(url, dl_path)
         downloaded_videos.append(mp4_filepath)
 
         # Convert video to mp3 if needed
